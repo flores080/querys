100bc65ccb26eec76b26bd0eab4977c73937e386
Fernando Flores <nbafernandoflores8@gmail.com>
2020-07-28 11:06:02 -0600

    Create weeek_startdate_finishdate.sql

---
+++ weeek_startdate_finishdate.sql
@@ -0,0 +1,10 @@
+#List all weeks on a range, display week, start date, finish date
+insert into SEMANA (ANIO, semana, fecha_inicio, fecha_fin)
+select 41 anio, week(selected_date, 6) semana, MIN(selected_date) fecha_inicio, MAX(selected_date) fecha_fin from
+(select adddate('1970-01-01',t4.i*10000 + t3.i*1000 + t2.i*100 + t1.i*10 + t0.i) selected_date from
+ (select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t0,
+ (select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t1,
+ (select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t2,
+ (select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t3,
+ (select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t4) v
+where selected_date between '2020-01-01' and '2020-12-31' group by semana;
