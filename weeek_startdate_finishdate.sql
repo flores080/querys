4411128963038cb0e825e9fccdb23f0042e83b80
Fernando Flores <nbafernandoflores8@gmail.com>
2021-02-17 10:03:25 -0600

    Update weeek_startdate_finishdate.sql

--- weeek_startdate_finishdate.sql
+++ weeek_startdate_finishdate.sql
@@ -8,3 +8,15 @@ select 41 anio, week(selected_date, 6) semana, MIN(selected_date) fecha_inicio, 
  (select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t3,
  (select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t4) v
 where selected_date between '2020-01-01' and '2020-12-31' group by semana;
+
+
+set @row_number = -1;
+insert into SEMANA (ANIO, semana, fecha_inicio, fecha_fin, CICLO)
+select 42 anio, (@`row_number`:=@`row_number`+1) semana, MIN(selected_date) fecha_inicio, MAX(selected_date) fecha_fin, 121 CICLO from
+    (select adddate('1970-01-01',t4.i*10000 + t3.i*1000 + t2.i*100 + t1.i*10 + t0.i) selected_date from
+    (select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t0,
+    (select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t1,
+    (select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t2,
+    (select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t3,
+    (select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t4) v
+where selected_date between '2021-01-01' and '2021-12-31' group by week(selected_date, 6), DATE_FORMAT(selected_date, '%m');
